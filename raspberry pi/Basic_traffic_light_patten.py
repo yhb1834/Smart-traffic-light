4d008681196e78483a1a44678dec81644c839ebc
Next Page 01 <pilotymc@gmail.com>
2021-05-14 09:53:49 +0000

    Add new file

---
+++ raspberry pi/Basic_traffic_light_patten.py
@@ -0,0 +1,9 @@
+while True:
+    print('A-C 직진')
+    print('A1,B4,C1,D4,a2,b1,c2,d1')
+    sleep(10)
+    print('A1,B4,C1,D4,a2,b1,c2,d1')
+    print(A1,B4,C1,D4,a2,b1,c2,d1)
+    print(A1,B4,C1,D4,a2,b1,c2,d1)
+    print(A1,B4,C1,D4,a2,b1,c2,d1)
+    print(A1,B4,C1,D4,a2,b1,c2,d1)
